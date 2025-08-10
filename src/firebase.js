78430fb275322fefe5550e0d87f9a7afd3696bda
siva <siva.anubothula@gmail.com>
2025-08-10 11:28:39 +0530

    Updated Commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu9GlnsXZavQbEh3WceWjhrjSioXdJwDE",
-  authDomain: "hirehub-d6eb6.firebaseapp.com",
-  projectId: "hirehub-d6eb6",
-  storageBucket: "hirehub-d6eb6.firebasestorage.app",
-  messagingSenderId: "1061761392409",
-  appId: "1:1061761392409:web:399d463a2a1f7fe0feef88",
-  measurementId: "G-KC7FHNGWD4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
